c35bd323dcba912f7270ee41d0f418d4fe471ce4
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-01 14:26:30 +0700

    parameter ref

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,17 +4,14 @@ class Program
 {
     static void Main()
     {
-        int b = add(1, 2, 3, 4, 5);
-        Console.WriteLine("b = {0}", b);
-        Console.WriteLine("1+5+8+4+1+6+6+8 = " + add(1, 5, 8, 4, 1, 6, 6, 8));
+        int val = 999;
+        refMethod(ref val);
+        Console.WriteLine(val);
         Console.ReadLine();
     }
 
-    static int add(params int[] i)
+    static void refMethod(ref int i)
     {
-        int sum = 0; ;
-        foreach (int j in i)
-            sum += j;
-        return sum;
+        i = i + 1;
     }
 }
