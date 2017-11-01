b29be42cca5a709dbfb3bee716a7f216d89afb17
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-01 16:26:25 +0700

    implicit type conversion

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main()
     {
-      
-            int a = 2;
-            string myString = a.ToString();
-            Console.WriteLine(myString);
+        int a = 2;
+        float f = 3.5f;
+        double d = 4.99d;
+        decimal money = 1254841354;
+        Console.WriteLine(a + " " + f + " " + d + " " + money);
 
     }
 }
