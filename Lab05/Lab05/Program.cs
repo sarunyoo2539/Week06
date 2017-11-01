92607e02218cc265a5351ea4ecde01d53e11c965
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-01 15:57:46 +0700

    Bitwise Operators

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,14 +4,16 @@ class Program
 {
     static void Main()
     {
-        bool a = false;
-        bool b = true;
-        Console.WriteLine(!a);
-        Console.WriteLine(a && b);
-        Console.WriteLine(a || b);
+        byte a = 3;
+        byte b = 5;
+
+        Console.WriteLine(a | b);
+        Console.WriteLine(a & b);
         Console.WriteLine(a ^ b);
-
-
+        Console.WriteLine(~a & b);
+        Console.WriteLine("{0}", Convert.ToString(a << 1).PadLeft(8, '0'));
+        Console.WriteLine("{0}", Convert.ToString(a << 2).PadLeft(8, '0'));
+        Console.WriteLine("{0}", Convert.ToString(a >> 1).PadLeft(8, '0'));
 
     }
 }
