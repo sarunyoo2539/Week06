f6da1af50378e965e2de93b0e80a9f5ab02720bc
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-01 15:35:08 +0700

    code snippet

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,21 +4,17 @@ class Program
 {
     static void Main()
     {
-        int x = 17;
-        double y = x / 4.0;
-        double z = y * y;
-        Console.WriteLine(y);
-        Console.WriteLine(z);
+        int x = 6;
+        int y = 4;
 
-        int a = 5;
-        int b = 4;
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (b++));
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (++b));
-        Console.WriteLine(a + b);
-        Console.WriteLine(14 / a);
-        Console.WriteLine(14 % a);
+        Console.WriteLine(y *= 2);
+        int z = y = 3;
+
+        Console.WriteLine(z);
+        Console.WriteLine(x |= 1);
+        Console.WriteLine(x += 3);
+        Console.WriteLine(x /= 2);
+
 
     }
 }
