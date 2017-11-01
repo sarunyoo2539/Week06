092bba8f745b7806cad1cc71d379826c710ac6d1
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-01 16:25:20 +0700

    ToString()

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -4,19 +4,10 @@ class Program
 {
     static void Main()
     {
-        try
-        {
-            double d = 5e9d;
-            Console.WriteLine(d);
-            int i = checked((int)d);
-            Console.WriteLine(i);
-
-        }
-
-        catch
-        {
-            Console.WriteLine("Error, null object assignment.");
-        }
+      
+            int a = 2;
+            string myString = a.ToString();
+            Console.WriteLine(myString);
 
     }
 }
