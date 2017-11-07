3317478325f7b856a5a3e2a047a6324ee6f88d68
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-07 16:52:36 +0700

    Event..Odd Number

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -2,13 +2,35 @@
 
 class Program
 {
-    static void Main()
+    static void Main(String[] args)
     {
-        int a = 2;
-        float f = 3.5f;
-        double d = 4.99d;
-        decimal money = 1254841354;
-        Console.WriteLine(a + " " + f + " " + d + " " + money);
-
+        try
+        {
+            Console.Write("Please enter value : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            EventCheck(val1);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
+    static int EventCheck(int val1)
+    {
+        if (val1 % 2 == 0)
+        {
+            Console.WriteLine("{0} is Event Number", val1);
+        }
+        else
+        {
+            Console.WriteLine("{0} is Odd Number", val1);
+        }
+        return 0;
+    }
+    
 }
+
