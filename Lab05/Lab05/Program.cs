b2e0ef9646d33acdf5227d5c3e2fcd6f45e865cb
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-07 17:14:44 +0700

    Weigh High

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,19 +6,33 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            Compare();
-            Console.ReadKey();           
-        }
-        static void Compare()
-        {
-            byte dec = 16;
-            for (int i = 1; i <= 100; i++)
+            try
             {
-                if ((i & dec) == dec)
-                {
-                    Console.Write(i + " ");
-                }
+                Console.Write("Please enter weight1 : ");
+                double w1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Please enter weight2 : ");
+                double w2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Please enter high : ");
+                double h = Convert.ToDouble(Console.ReadLine());
+
+                Area(w1, w2, h);
             }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        static void Area(double w1, double w2, double h)
+        {
+            double a = ((w1 + w2) * h) * 1 / 2;
+            Console.WriteLine("result = {0} cm^2", a);
         }
     }
 }
